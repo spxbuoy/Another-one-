644535440c53895d8ee5ceae0f3876ca9127c2e4
sayomais <sayomais@outlook.com>
2025-05-20 18:31:55 +0500

    Update ss1.py

--- plugins/gates/ss1.py
+++ plugins/gates/ss1.py
@@ -79,7 +79,7 @@ async def cmd_ss1(client, message):
                 "card": fullcc,
                 "product_url": "https://musicworksunlimited.com/products/cuddle-up-cuddle-cub-music-single",
                 "email": None,
-                "proxy": "proxy.speedproxies.net:12321:Indexui184a999e:4fba9e5235e8_country-us",
+                "proxy": "proxy.rampageproxies.com:5000:package-1111111-country-us-city-bloomington-region-indiana:5671nuWwEPrHCw2t",
                 "ship_address": None,
                 "is_shippable": False
             }
