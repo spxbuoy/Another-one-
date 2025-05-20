6b31f6dc57121ddbb9fed2fd3cb88fc52c042ddf
sayomais <sayomais@outlook.com>
2025-05-20 18:28:04 +0500

    Update mass_shopify_func.py

--- plugins/gates/func/mass_shopify_func.py
+++ plugins/gates/func/mass_shopify_func.py
@@ -10,7 +10,7 @@ async def shopify_func(_, cc, cvv, mes, ano):
             "card": fullcc,
             "product_url": "https://godless.com/collections/the-drop-all-new-shit/products/dark-corners-on-flat-surfaces-by-adler-tittle",
             "email": None,
-            "proxy": "proxy.speedproxies.net:12321:Indexui184a999e:4fba9e5235e8_cuntry-us",
+            "proxy": "proxy.rampageproxies.com:5000:package-1111111-country-us-city-bloomington-region-indiana:5671nuWwEPrHCw2t",
             "ship_address": None,
             "is_shippable": False
         }
